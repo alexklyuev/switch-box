1b8787b57382bca36fedba0ebc679982831477fc
AlexKluev <clesign@yandex.ru>
2017-05-03 22:27:22 +0300

    async test for sync match, match async in ready

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 ## Extendable pattern matching library for typescript
 
-### Basic usage
+### Synchronous match
+ - only first successful case would be ran, others would be skipped
+ - use only synchronous matching functions, otherwise more then one case could be ran
+ - `default` should be the last method in chain, otherwise only it would be ran
 
 ```typescript
-import { match } from './src/match';
+import { match } from 'switch-box';
 
 match(42)
 .case<number>(
@@ -17,6 +20,34 @@ match(42)
 .default(
   value => console.info(`this is ${typeof value}`)
 );
+```
+
+### Asynchronous match
+ - match functions could be asynchronous
+ - cases would be run one after another until successful case
+ - it could be only one successful case, first would be ran, others would be skipped
+
+```typescript
+import { matchAsync } from 'switch-box';
+
+matchAsync(42)
+.case<number>(
+  (value, matcher) => {
+    setTimeout(() => {
+      matcher(typeof value === 'number');
+    }, 160);
+  },
+  value => console.info('this is number')
+)
+.case<string>(
+  (value, matcher) => {
+    setTimeout(() => {
+      matcher(typeof value === 'string');
+    }, 160);
+  },
+  value => console.info('this is string')
+)
+;
 ```
 
 ### JS
--- spec/match/match.spec.ts
+++ spec/match/match.spec.ts
@@ -3,43 +3,91 @@ import { match } from "../../";
 
 describe('Base sync match', () => {
 
-  it('first true case', done => {
-    match(42)
-    .case<number>(
-      (value, matcher) => {matcher(typeof value === 'number')},
-      value => done(),
-    )
-    .case<string>(
-      (value, matcher) => {matcher(typeof value === 'string')},
-      value => done.fail(),
-    );
+  describe('syncronous matchers, ', () => {
+
+    it('first true case', done => {
+      match(42)
+      .case<number>(
+        (value, matcher) => {matcher(typeof value === 'number')},
+        value => done(),
+      )
+      .case<string>(
+        (value, matcher) => {matcher(typeof value === 'string')},
+        value => done.fail(),
+      )
+      .default(
+        value => done.fail(),
+      )
+      ;
+    });
+
+    it('second true case', done => {
+      match('42')
+      .case<number>(
+        (value, matcher) => {matcher(typeof value === 'number')},
+        value => done.fail(),
+      )
+      .case<string>(
+        (value, matcher) => {matcher(typeof value === 'string')},
+        value => done(),
+      )
+      .default(
+        value => done.fail(),
+      )
+      ;
+    });
+
+    it('default', done => {
+      match(true)
+      .case<number>(
+        (value, matcher) => {matcher(typeof value === 'number')},
+        value => done.fail(),
+      )
+      .case<string>(
+        (value, matcher) => {matcher(typeof value === 'string')},
+        value => done.fail(),
+      )
+      .default(
+        value => done(),
+      )
+      ;
+    });
+
   });
 
-  it('second true case', done => {
-    match('42')
-    .case<number>(
-      (value, matcher) => {matcher(typeof value === 'number')},
-      value => done.fail(),
-    )
-    .case<string>(
-      (value, matcher) => {matcher(typeof value === 'string')},
-      value => done(),
-    );
-  });
+  describe('asyncronous mathcers, ', () => {
 
-  it('default', done => {
-    match(true)
-    .case<number>(
-      (value, matcher) => {matcher(typeof value === 'number')},
-      value => done.fail(),
-    )
-    .case<string>(
-      (value, matcher) => {matcher(typeof value === 'string')},
-      value => done.fail(),
-    )
-    .default(
-      value => done(),
-    );
+    it('in case of async matchers, default case would be run if it runs before matcher', done => {
+      const vals = [false, false];
+      new Promise(resolve => {
+        match(42)
+        .case<number>(
+          (value, matcher) => {
+            setTimeout(() => {
+              matcher(typeof value === 'number');
+            }, 160);
+          },
+          value => {
+            vals[0] = true;
+            resolve();
+          },
+        )
+        .case<string>(
+          (value, matcher) => {matcher(typeof value === 'string')},
+          value => done.fail(),
+        )
+        .default(
+          value => {
+            vals[1] = true;
+          },
+        )
+        ;
+      }).then(() => {
+        expect(vals.every(v => v === true)).toBe(true);
+        done();
+      });
+    });
+    
   });
 
 });
